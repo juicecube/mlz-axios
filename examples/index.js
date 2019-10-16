ab8c04776612c0e5edd894156ed19c1e17456ae9
wanghao <wanghao1@codemao.cn>
2019-10-16 17:54:03 +0800

    feat:支持设置单个实例拦截器/静态方法全局设置token

--- examples/index.js
+++ examples/index.js
@@ -1,39 +1,46 @@
-import Http from '../src/index'
-Http.setToken(2, 'aaa')
-// Http.setDefaultConf({
-//   headers: {
-//     post: 'application/json'
-//   }
-// })
-Http.setReqInterceptor((config) => {
-  console.log('请求前config', config)
-}, (err) => {
-  console.log('请求前err', err)
-})
-Http.setResInterceptor((res) => {
-  console.log('请求后res', res)
-}, (err) => {
-  console.log('请求前err', err)
-})
-const httpIns = new Http('http://localhost:8080')
-httpIns.post('/base/post',
-  {
-    a: 1,
-    b: 2
-  },
-).then((res) => {
-  console.log(res)
-})
+import Http from "../src/index";
+const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0NTAsInVzZXJfdHlwZSI6ImFkbWluIiwianRpIjoiNjVlMTY1NzEtOTliNy00YzBiLTg0ODktYjhiYzNkNTE2ZGY3IiwiaWF0IjoxNTcxMDQyNzM2fQ.cHA8rBDMZFj7P1a6UWKjINtsYOOt77ROAhK0kKZBx6Y'
+const type = 3;
+const httpIns = new Http("https://backend-dev.codemao.cn");
+const httpIns2 = new Http("https://dev-api-crm-codemaster.codemao.cn");
 
-const httpIns1 = new Http('http://localhost:9000')
-httpIns1.get('/error/timeout').then((res) => {
-  console.log(res)
-})
-httpIns.post('/base/post',
-  {
-    a: 1,
-    b: 2
+Http.setAuthorizationTypeOrToken("authorization_type", type, "Authorization", token);
+
+Http.setReqInterceptor(
+  config => {
+    console.log("请求前config", config);
+    return config;
   },
-).then((res) => {
-  console.log(res)
-})
+  err => {
+    console.log("请求前err", err);
+    return Promise.reject(err);
+  },
+  "https://backend-dev.codemao.cn"
+);
+Http.setResInterceptor(
+  res => {
+    console.log("请求后res", res);
+    return res;
+  },
+  err => {
+    console.log("请求后err", err);
+    return Promise.reject(err);
+  }
+);
+
+httpIns
+  .post("/tiger/lesson/ticket/give-out", {
+    amount: "1",
+    student_id: "1000824602",
+    ticket_type: "101"
+  })
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+httpIns2.get("/admin/crm/head-teachers/statistics/card", { params: { card_type: 4 } }).then(res => {
+  console.log(res.data);
+});
--- src/mlz_axios.ts
+++ src/mlz_axios.ts
@@ -1,6 +1,6 @@
-import axios, { 
-  AxiosRequestConfig, 
-  CancelTokenStatic, 
+import axios, {
+  AxiosRequestConfig,
+  CancelTokenStatic,
   CancelTokenSource,
   AxiosPromise,
   AxiosResponse,
@@ -14,35 +14,37 @@ const DEFAULT_CONFIG:AxiosRequestConfig = {
 }
 
 export class Http {
-  authorizationType;
-  authorizationToken:string = '';
-  cancelToken:CancelTokenStatic = axios.CancelToken;
-  source:CancelTokenSource = this.cancelToken.source();
-  public axiosIns:AxiosInstance;
-  static INSTANCES:{[key:string]: AxiosInstance} = {};
-  constructor(baseUrl:string, config:AxiosRequestConfig = {}) {
+  private cancelToken: CancelTokenStatic = axios.CancelToken;
+  private source: CancelTokenSource = this.cancelToken.source();
+  private axiosIns: AxiosInstance;
+
+  static authorizationTypeKey:string = 'Authorization';
+  static authorizationTokenKey:string = 'authorization_type';
+  static authorizationType:number;
+  static authorizationToken: string = "";
+  static INSTANCES: { [key: string]: AxiosInstance } = {};
+
+  constructor(baseUrl: string, config: AxiosRequestConfig = {}) {
     const axiosIns = axios.create({
       baseURL: baseUrl,
       ...DEFAULT_CONFIG,
-      ...config,
+      ...config
     });
     Http.INSTANCES[baseUrl] = axiosIns;
     this.axiosIns = axiosIns;
   }
-  setAuthorizationTypeOrToken(type, token) {
-    this.authorizationToken = token;
-    this.authorizationType = type;
-  }
-  public request(opt:AxiosRequestConfig) {
-    const _opt = Object.assign(null, opt);
+
+  private request(opt: AxiosRequestConfig) {
+    const _opt = Object.assign({}, opt);
     _opt.headers = {
       ..._opt.headers,
-      authorization_type: this.authorizationType,
-      authorization_token: this.authorizationToken,
+      [Http.authorizationTypeKey]: Http.authorizationType,
+      [Http.authorizationTokenKey]: Http.authorizationToken
     };
     _opt.cancelToken = this.source.token;
     return this.axiosIns.request(_opt);
   }
+
   public abort() {
     this.source.cancel('API abort.')
   }
@@ -58,9 +60,6 @@ export class Http {
       method: 'post',
       url,
       data,
-      headers: {
-        'Content-type': 'application/x-www-form-urlencoded'
-      },
       ...configs,
     })
   }
@@ -88,20 +87,38 @@ export class Http {
     })
   }
   // 下面是静态方法
+  static setAuthorizationTypeOrToken(typeKey:string, typeValue:number, tokenKey:string, tokenValue:string) {
+    Http.authorizationTypeKey = typeKey;
+    Http.authorizationTokenKey = tokenKey;
+    Http.authorizationToken = tokenValue;
+    Http.authorizationType = typeValue;
+  }
+
   static getInstances(key:string) : AxiosInstance {
     return Http.INSTANCES[key];
   }
-  static setReqInterceptor (resolve?:(config:AxiosRequestConfig) => AxiosRequestConfig, reject?:(error:any) => void) {
-    for (let key in Http.INSTANCES) {
+
+  static setReqInterceptor (resolve?:(config:AxiosRequestConfig) => AxiosRequestConfig, reject?:(error:any) => void, key?:string) {
+    if(key) {
       const instance = Http.INSTANCES[key];
       instance.interceptors.request.use(resolve, reject);
+    } else {
+      for (let key in Http.INSTANCES) {
+        const instance = Http.INSTANCES[key];
+        instance.interceptors.request.use(resolve, reject);
+      }
     }
   }
-  static setResInterceptor (resolve?:(res:AxiosResponse) => AxiosResponse, reject?:(error) => any) {
-    for (let key in Http.INSTANCES) {
+
+  static setResInterceptor (resolve?:(res:AxiosResponse) => AxiosResponse, reject?:(error) => any, key?:string) {
+    if(key) {
       const instance = Http.INSTANCES[key];
       instance.interceptors.response.use(resolve, reject);
+    } else {
+      for (let key in Http.INSTANCES) {
+        const instance = Http.INSTANCES[key];
+        instance.interceptors.response.use(resolve, reject);
+      }
     }
   }
 }
-
