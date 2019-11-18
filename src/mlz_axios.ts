e59365dc36e116391f2db25391acbc0fe7b99f61
wangzhen <wangzhen@codemao.cn>
2019-11-18 14:08:01 +0800

    fix: 某个请求传入的config可以覆盖原有的config & 双引号改成单引号

--- src/mlz_axios.ts
+++ src/mlz_axios.ts
@@ -1,11 +1,10 @@
 import axios, {
   AxiosRequestConfig,
-  CancelTokenStatic,
   CancelTokenSource,
   AxiosPromise,
   AxiosResponse,
   AxiosInstance
-} from "axios";
+} from 'axios';
 
 const DEFAULT_CONFIG: AxiosRequestConfig = {
   timeout: 5000,
@@ -16,18 +15,17 @@ const DEFAULT_CONFIG: AxiosRequestConfig = {
 export class Http {
   private isSetInstancesToken: boolean = false;
 
-  public cancelToken: CancelTokenStatic = axios.CancelToken;
   public axiosIns: AxiosInstance;
-  public source: CancelTokenSource = this.cancelToken.source();
-  public authorizationTypeKey: string = "Authorization";
-  public authorizationTokenKey: string = "authorization_type";
+  public source: CancelTokenSource = axios.CancelToken.source();
+  public authorizationTypeKey: string = 'Authorization';
+  public authorizationTokenKey: string = 'authorization_type';
   public authorizationTypeValue: number;
-  public authorizationTokenValue: string = "";
+  public authorizationTokenValue: string = '';
 
-  static authorizationTypeKey: string = "Authorization";
-  static authorizationTokenKey: string = "authorization_type";
+  static authorizationTypeKey: string = 'Authorization';
+  static authorizationTokenKey: string = 'authorization_type';
   static authorizationTypeValue: number;
-  static authorizationTokenValue: string = "";
+  static authorizationTokenValue: string = '';
   static INSTANCES: { [key: string]: AxiosInstance } = {};
   static INSTANCES_REQUEST_INTERCEPTORS: { [key: string]: number } = {};
   static INSTANCES_RESPONSE_INTERCEPTORS: { [key: string]: number } = {};
@@ -65,9 +63,9 @@ export class Http {
         };
       } else {
         _opt.headers = {
-          ..._opt.headers,
           [this.authorizationTypeKey]: this.authorizationTypeValue,
-          [this.authorizationTokenKey]: this.authorizationTokenValue
+          [this.authorizationTokenKey]: this.authorizationTokenValue,
+          ..._opt.headers,
         };
       }
     } else {
@@ -77,9 +75,9 @@ export class Http {
         };
       } else {
         _opt.headers = {
-          ..._opt.headers,
           [Http.authorizationTypeKey]: Http.authorizationTypeValue,
-          [Http.authorizationTokenKey]: Http.authorizationTokenValue
+          [Http.authorizationTokenKey]: Http.authorizationTokenValue,
+          ..._opt.headers,
         };
       }
       
@@ -89,18 +87,18 @@ export class Http {
   }
 
   public abort() {
-    this.source.cancel("API abort.");
+    this.source.cancel('API abort.');
   }
   public get(url: string, configs?: AxiosRequestConfig) : AxiosPromise {
     return this.request({
-      method: "get",
+      method: 'get',
       url,
       ...configs
     });
   }
   public post(url: string, data?: any, configs?: AxiosRequestConfig): AxiosPromise {
     return this.request({
-      method: "post",
+      method: 'post',
       url,
       data,
       ...configs
@@ -108,7 +106,7 @@ export class Http {
   }
   public put(url: string, data?: any, configs?: AxiosRequestConfig): AxiosPromise {
     return this.request({
-      method: "put",
+      method: 'put',
       url,
       data,
       ...configs
@@ -116,7 +114,7 @@ export class Http {
   }
   public patch(url: string, data?: any, configs?: AxiosRequestConfig): AxiosPromise {
     return this.request({
-      method: "patch",
+      method: 'patch',
       url,
       data,
       ...configs
@@ -124,7 +122,7 @@ export class Http {
   }
   public delete(url: string, configs?: AxiosRequestConfig): AxiosPromise {
     return this.request({
-      method: "delete",
+      method: 'delete',
       url,
       ...configs
     });
